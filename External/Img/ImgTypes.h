4371364dff7e9ee7ccb9dd984cbc734513ce8337
Neil Richardson <richyneil@gmail.com>
2011-04-17 20:05:01 +1000

    Added font import/export.

--- External/Img/ImgTypes.h
+++ External/Img/ImgTypes.h
@@ -20,17 +20,62 @@
 #include "BcDebug.h"
 #include "BcMemory.h"
 #include "BcString.h"
+#include "BcMath.h"
 
 //////////////////////////////////////////////////////////////////////////
 // ImgColour
-class ImgColour
+struct ImgColour
 {
-public:
 	BcU8		B_;
 	BcU8		G_;
 	BcU8		R_;
 	BcU8		A_;
+	
+	bool operator == ( const ImgColour& Other ) const
+	{
+		return R_ == Other.R_ && G_ == Other.G_ && B_ == Other.B_ && A_ == Other.A_;
+	}
+
+	bool operator != ( const ImgColour& Other ) const
+	{
+		return R_ != Other.R_ || G_ != Other.G_ || B_ != Other.B_ || A_ != Other.A_;
+	}
 };
+
+//////////////////////////////////////////////////////////////////////////
+// ImgRect
+struct ImgRect
+{
+	BcU32 X_;
+	BcU32 Y_;
+	BcU32 W_;
+	BcU32 H_;
+	
+	BcBool isInsideOf( const ImgRect& Other ) const
+	{
+		if( ( X_ >= Other.X_ && ( X_ + W_ ) <= ( Other.X_ + Other.W_  ) ) &&
+		    ( Y_ >= Other.Y_ && ( Y_ + H_ ) <= ( Other.Y_ + Other.H_  ) ) )
+		{
+			return BcTrue;
+		}
+		return BcFalse;
+	}
+	
+	BcBool isOutsideOf( const ImgRect& Other ) const
+	{
+		if( ( ( X_ + W_ ) < Other.X_ || X_ > ( Other.X_ + Other.W_ ) ) ||
+		    ( ( Y_ + H_ ) < Other.Y_ || Y_ > ( Other.Y_ + Other.H_ ) ) )
+		{
+			return BcTrue;
+		}
+		return BcFalse;
+	}
+};
+
+typedef std::vector< ImgRect > ImgRectList;
+typedef ImgRectList::iterator ImgRectListIterator;
+typedef ImgRectList::const_iterator ImgRectListConstIterator;
+
 
 //////////////////////////////////////////////////////////////////////////
 // Format
