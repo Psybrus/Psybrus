35cf2c4caf8b9300ccf1813307411d1ab3502ba8
Neil Richardson <richyneil@gmail.com>
2011-04-21 00:59:53 +1000

    - Parameter binding fixes in GL implementation. - Clearing depth buffer by default in RsFrameGL (need to improve this stuff soon)

--- Engine/Source/Platforms/OSX/MainMac.cpp
+++ Engine/Source/Platforms/OSX/MainMac.cpp
@@ -44,13 +44,18 @@ int main( int argc, char* argv[] )
 #include "ScnCanvas.h"
 #include "ScnShader.h"
 #include "ScnFont.h"
+#include "ScnPackage.h"
+
 OsEventCore::Delegate DelegateQuit;
 SysSystemEvent::Delegate DelegateRender;
 SysSystemEvent::Delegate DelegateUpdate;
 SysSystemEvent::Delegate DelegateRemoteOpened;
 
 ScnTextureRef Texture;
+
 ScnModelRef Model;
+ScnModelInstanceRef ModelInstance;
+
 ScnCanvasRef Canvas;
 ScnMaterialRef MaterialRef;
 ScnMaterialInstanceRef MaterialInstanceRef;
@@ -63,6 +68,8 @@ ScnFontRef EagleFontRef;
 ScnFontInstanceRef BaroqueFontInstanceRef;
 ScnFontInstanceRef EagleFontInstanceRef;
 
+ScnPackageRef PackageRef;
+
 eEvtReturn doQuit( EvtID ID, const OsEventCore& Event )
 {
 	// Stop all systems.
@@ -74,31 +81,43 @@ eEvtReturn doQuit( EvtID ID, const OsEventCore& Event )
 
 eEvtReturn doUpdate( EvtID ID, const SysSystemEvent& Event )
 {
-	if( MaterialRef.isValid() == BcFalse )
+	if( PackageRef.isValid() == BcFalse )
 	{
 		CsCore::pImpl()->registerResource< ScnTexture >();
 		CsCore::pImpl()->registerResource< ScnMaterial >();
 		CsCore::pImpl()->registerResource< ScnMaterialInstance >();
 		CsCore::pImpl()->registerResource< ScnModel >();
+		CsCore::pImpl()->registerResource< ScnModelInstance >();
 		CsCore::pImpl()->registerResource< ScnCanvas >();
 		CsCore::pImpl()->registerResource< ScnShader >();
 		CsCore::pImpl()->registerResource< ScnFont >();
 		CsCore::pImpl()->registerResource< ScnFontInstance >();
+		CsCore::pImpl()->registerResource< ScnPackage >();
 
+		CsCore::pImpl()->requestResource( "boot", PackageRef );
+	}
+	if( PackageRef.isReady() && DefaultMaterialRef.isValid() == BcFalse )
+	{
+		// These should all be ready!
 		CsCore::pImpl()->requestResource( "default", DefaultMaterialRef );
 		CsCore::pImpl()->requestResource( "font", MaterialRef );
 		CsCore::pImpl()->requestResource( "baroque", BaroqueFontRef );
-		CsCore::pImpl()->requestResource( "eaglegtiii", EagleFontRef );
-	}
-	if( DefaultMaterialRef.isReady() && MaterialRef.isReady() && BaroqueFontRef.isReady() && EagleFontRef.isReady() && Canvas.isValid() == BcFalse )
-	{
+		CsCore::pImpl()->requestResource( "eaglegtiii", EagleFontRef );		
+		CsCore::pImpl()->requestResource( "model", Model );
+		
+		BcAssert( DefaultMaterialRef.isReady() );
+		BcAssert( MaterialRef.isReady() );
+		//BcAssert( BaroqueFontRef.isReady() );
+		//BcAssert( EagleFontRef.isReady() );
+
 		if( DefaultMaterialRef->createInstance( "default_materialinstance", DefaultMaterialInstanceRef, scnSPF_DEFAULT ) )
 		{
 			CsCore::pImpl()->createResource( "canvas", Canvas, 1024, MaterialInstanceRef );
 		}
-		MaterialRef->createInstance( "font_materialinstance", MaterialInstanceRef, scnSPF_DEFAULT );
-		BaroqueFontRef->createInstance( "baroque_fontinstance", BaroqueFontInstanceRef, MaterialRef );
-		EagleFontRef->createInstance( "eaglegtiii_fontinstance", EagleFontInstanceRef, MaterialRef );
+		//MaterialRef->createInstance( "font_materialinstance", MaterialInstanceRef, scnSPF_DEFAULT );
+		//BaroqueFontRef->createInstance( "baroque_fontinstance", BaroqueFontInstanceRef, MaterialRef );
+		//EagleFontRef->createInstance( "eaglegtiii_fontinstance", EagleFontInstanceRef, MaterialRef );
+		Model->createInstance( "model_modelinstance", ModelInstance );
 	}
 		
 	// No more updates please.
@@ -106,74 +125,6 @@ eEvtReturn doUpdate( EvtID ID, const SysSystemEvent& Event )
 
 	return evtRET_PASS;
 }
-
-BcChar* VertexShader = 
-"attribute vec4 aPosition;						"
-"attribute vec4 aColour;						"
-"varying vec4 vColour;							"
-"void main()									"
-"{												"
-"	gl_Position = aPosition;					"
-"	vColour = aColour;							"
-"}												";
-
-
-BcChar* FragmentShader =
-"varying vec4 vColour;							"
-"void main()									"
-"{												"
-"	gl_FragColor = vColour;						"
-"}												";
-
-
-RsIndexBuffer* pIndexBuffer = NULL;
-RsVertexBuffer* pVertexBuffer = NULL;
-RsPrimitive* pPrimitive = NULL;
-RsShader* pVertexShader = NULL;
-RsShader* pFragmentShader = NULL;
-RsProgram* pProgram = NULL;
-
-// Test node.
-class TestNode: public RsRenderNode
-{
-public:
-	TestNode( RsRenderSort Sort )
-	{
-		Sort_ = Sort;	
-	}
-	
-	void render()
-	{
-		static BcReal Tick = 0.0f;
-		Tick += 0.001f;
-		
-		pProgram->bind( NULL );
-		
-		/*
-		glBegin( GL_LINE_STRIP );
-		glVertex3f( -0.25f, -0.25f, 0.1f );
-		glVertex3f(  0.25f, -0.25f, 0.1f );
-		glVertex3f(  0.25f,  0.25f, 0.1f );
-		glVertex3f( -0.25f,  0.25f, 0.1f );
-		glVertex3f( -0.25f, -0.25f, 0.1f );
-		glEnd();
-		 */
-	}
-};
-
-struct TVertex
-{
-	BcF32 X, Y, Z;
-};
-BcU16 Indices[] = { 0, 1, 2, 3, 0 };
-TVertex Vertices[] =
-{
-	{ -0.25f, -0.25f, 0.1f },
-	{  0.25f, -0.25f, 0.1f },
-	{  0.25f,  0.25f, 0.1f },
-	{ -0.25f,  0.25f, 0.1f },
-	{ -0.25f, -0.25f, 0.1f },
-};
 
 eEvtReturn doRender( EvtID ID, const SysSystemEvent& Event )
 {
@@ -186,8 +137,10 @@ eEvtReturn doRender( EvtID ID, const SysSystemEvent& Event )
 	BcMat4d View;
 	
 	static BcReal Ticker = 0.0f;
-	Ticker += 0.001f;
-	View.rotation( BcVec3d( 0.0f, 0.0f, Ticker ) );
+	Ticker += 0.002f;
+	View.rotation( BcVec3d( Ticker * 3.0f, Ticker * 2.0f, Ticker ) );
+	
+	View.translation( BcVec3d( 0.0f, 0.0f, 100.0f ) );
 	
 	BcReal W = 1280.0f * 0.5f;
 	BcReal H = 720.0f * 0.5f;
@@ -202,12 +155,13 @@ eEvtReturn doRender( EvtID ID, const SysSystemEvent& Event )
 	//*
 	if( Canvas.isReady() )
 	{
+		/*
 		BcMat4d Rotate;
 		BcMat4d Scale;
 		BcMat4d Translate;
 		
 		Translate.translation( BcVec3d( 0.0f, 32.0f, 0.0f ) );
-		BcReal ScaleValue = ( BcSin( Ticker * 5.0f ) + 1.0f ) * 3.0f + 0.5f;
+		BcReal ScaleValue = 0.1f;//( BcSin( Ticker * 5.0f ) + 1.0f ) * 3.0f + 0.5f;
 		Scale.scale( BcVec3d( ScaleValue, ScaleValue, ScaleValue ) );
 		Rotate.rotation( BcVec3d( 0.0f, 0.0f, Ticker ) );
 	
@@ -220,9 +174,6 @@ eEvtReturn doRender( EvtID ID, const SysSystemEvent& Event )
 		Canvas->pushMatrix( Rotate );		
 		Canvas->pushMatrix( Scale );		
 		
-		Canvas->setMaterialInstance( MaterialInstanceRef );
-		Canvas->drawBox( BcVec2d( 0, 0 ), BcVec2d( W, -H ), RsColour::WHITE, 0 );
-
 		BaroqueFontInstanceRef->draw( Canvas, "0123456789+-/\\!@#$%^&*()" );
 		Canvas->setMaterialInstance( DefaultMaterialInstanceRef );
 		Canvas->drawLine( BcVec2d( 0, 0 ), BcVec2d( W, 0 ), RsColour::RED, 1 );	
@@ -237,7 +188,18 @@ eEvtReturn doRender( EvtID ID, const SysSystemEvent& Event )
 		Canvas->drawLine( BcVec2d( 0, 0 ), BcVec2d( 0, -32 ), RsColour::RED, 1 );	
 		Canvas->popMatrix();
 		
-		Canvas->render( pFrame, RsRenderSort( 0 ) );
+		//Canvas->render( pFrame, RsRenderSort( 0 ) );
+		 */
+	
+		Viewport.perspProj( BcPI * 0.125f, W / H, 0.1f, 1000.0f );
+ 		Viewport.view( View );
+
+		BcMat4d ModelTransform;
+		
+		ModelInstance->setTransform( 0, Viewport.viewProjection() * ModelTransform ); 
+		ModelInstance->update();
+		
+		ModelInstance->render( pFrame, RsRenderSort( 0 ) );
 	}
 	// */
 	
@@ -256,6 +218,18 @@ eEvtReturn doRemoteOpened( EvtID, const SysSystemEvent& Event )
 
 void PsyGameInit( SysKernel& Kernel )
 {
+	BcAssert( sizeof( BcU8 ) == 1 );
+	BcAssert( sizeof( BcS8 ) == 1 );
+	BcAssert( sizeof( BcU16 ) == 2 );
+	BcAssert( sizeof( BcS16 ) == 2 );
+	BcAssert( sizeof( BcU32 ) == 4 );
+	BcAssert( sizeof( BcS32 ) == 4 );
+	BcAssert( sizeof( BcU64 ) == 8 );
+	BcAssert( sizeof( BcS64 ) == 8 );
+	BcAssert( sizeof( BcF32 ) == 4 );
+	BcAssert( sizeof( BcF64 ) == 8 );
+
+	
 	BcPrintf( "Entering PsyGameInit.\n" );
 	
 	//
--- Engine/Source/Platforms/OSX/Renderer/RsFrameGL.cpp
+++ Engine/Source/Platforms/OSX/Renderer/RsFrameGL.cpp
@@ -76,8 +76,8 @@ class RsRenderTargetNode: public RsRenderNode
 public:
 	void render()
 	{
-		glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
-		glClear( GL_COLOR_BUFFER_BIT );		
+		glClearColor( 0.0f, 0.0f, 0.2f, 1.0f );
+		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );		
 	}
 };
 
