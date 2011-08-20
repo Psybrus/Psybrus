3a0159831d9f638862ccf2ad86d957cf3447d275
Neil Richardson <richyneil@gmail.com>
2011-08-20 11:22:34 +1000

    stuffs before LD

---
+++ Engine/Source/Shared/System/Renderer/RsFrameBufferGL.h
@@ -0,0 +1,26 @@
+/**************************************************************************
+*
+* File:		RsFrameBufferGL.h
+* Author:	Neil Richardson 
+* Ver/Date:	16/08/11
+* Description:
+*		
+*		
+*
+*
+* 
+**************************************************************************/
+
+#ifndef __RsFrameBufferGL_H__
+#define __RsFrameBufferGL_H__
+
+#include "BcTypes.h"
+
+//////////////////////////////////////////////////////////////////////////
+// RsFrameBufferGL
+
+
+
+#endif
+
+
---
+++ Engine/Source/Shared/System/Renderer/RsRenderBufferGL.h
@@ -0,0 +1,26 @@
+/**************************************************************************
+*
+* File:		RsRenderBufferGL.h
+* Author:	Neil Richardson 
+* Ver/Date:	16/08/11
+* Description:
+*		
+*		
+*
+*
+* 
+**************************************************************************/
+
+#ifndef __RsRenderBufferGL_H__
+#define __RsRenderBufferGL_H__
+
+#include "BcTypes.h"
+
+//////////////////////////////////////////////////////////////////////////
+// RsRenderBufferGL
+
+
+
+#endif
+
+
