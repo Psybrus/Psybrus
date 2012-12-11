9069b993fe791d57d38a3ec34a472834de77409c
Neil Richardson <richyneil@gmail.com>
2012-12-11 23:10:15 +1100

    Mongoose to 7DFPS.

--- 7DFPSGame/premake4.lua
+++ 7DFPSGame/premake4.lua
@@ -23,6 +23,7 @@ project "7FPSGame"
    			"External_glew",
    			"External_jsoncpp",
    			"External_libb64",
+            "External_mongoose",
    			"External_ogg",
    			"External_png",
             "External_portaudio",
