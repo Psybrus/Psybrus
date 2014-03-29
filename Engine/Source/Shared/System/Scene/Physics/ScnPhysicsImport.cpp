6afbc56e6d7779d6539ecbec7306ad3929514a9d
Neil Richardson <richyneil@gmail.com>
2014-03-29 11:54:20 +1100

    basic physics shell.

---
+++ Engine/Source/Shared/System/Scene/Physics/ScnPhysicsImport.cpp
@@ -0,0 +1,62 @@
+/**************************************************************************
+*
+* File:		ScnPhysicsImport.cpp
+* Author:	Neil Richardson 
+* Ver/Date: 25/02/13
+* Description:
+*		
+*		
+*
+*
+* 
+**************************************************************************/
+
+#include "ScnPhysicsImport.h"
+
+#include "ScnPhysicsFileData.h"
+
+#ifdef PSY_SERVER
+
+//////////////////////////////////////////////////////////////////////////
+// Ctor
+ScnPhysicsImport::ScnPhysicsImport()
+{
+	
+}
+
+//////////////////////////////////////////////////////////////////////////
+// import
+BcBool ScnPhysicsImport::import( class CsPackageImporter& Importer, const Json::Value& Object )
+{
+	// TODO: Clean out into individual classes...hack for easy for now.
+	pImporter_ = &Importer;
+
+	BcStream HeaderStream;
+
+	if( BcStrCompare( Object[ "type" ].asCString(), "ScnPhysicsBoxCollisionShape" ) )
+	{
+		ScnPhysicsBoxCollisionShapeHeader Header;
+	
+		Header.HalfExtents_ = BcVec3d( Object[ "extents" ].asCString() );
+
+		HeaderStream << Header;
+	}
+	else if( BcStrCompare( Object[ "type" ].asCString(), "ScnPhysicsSphereCollisionShape" ) )
+	{
+		ScnPhysicsSphereCollisionShapeHeader Header;
+	
+		Header.Radius_ = static_cast< BcF32 >( Object[ "radius" ].asDouble() );
+
+		HeaderStream << Header;
+	}
+	else
+	{
+		return BcFalse;
+	}
+
+	Importer.addChunk( BcHash( "header" ), HeaderStream.pData(), HeaderStream.dataSize() );
+
+	return BcTrue;
+}
+
+#endif
