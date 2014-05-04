7cfb92ad3d4f39bc9cd79dec2f8a2cf5bf227551
Neil Richardson <neilo@neilo.gd>
2014-05-05 00:13:32 +1000

    Changed to take vertex input, and properties separately.

--- Dist/Content/Engine/Psybrus.hlsl
+++ Dist/Content/Engine/Psybrus.hlsl
@@ -1,18 +1,30 @@
 #include "PsybrusUniforms.hlsl"
 
-#if defined( PERM_MESH_SKINNED )
+#if defined( PERM_MESH_STATIC )
+/**
+ * Make a world space vertex.
+ * @param _o Output vertex. Should be float4.
+ * @param _v Input vertex. Should be float4.
+ * @param _p Input properties. Unused.
+ */
+#  define PSY_MAKE_WORLD_SPACE_VERTEX( _o, _v, _p ) 													\
+		_o = mul( uWorldTransform, _v );																\
+
+
+#elif defined( PERM_MESH_SKINNED )
 
 /**
  * Make a world space vertex.
  * @param _o Output vertex. Should be float4.
- * @param _v Input vertex. Should be a structure containing Position_, BlendIndices_, and BlendWeights_.
+ * @param _v Input vertex. Should be float4.
+ * @param _p Input properties. Should be a structure containing BlendIndices_, and BlendWeights_.
  */
-#  define PSY_MAKE_WORLD_SPACE_VERTEX( _o, _v ) 																\
-		_o = float4( 0.0, 0.0, 0.0, 0.0 );																		\
-		_o += mul( uBoneTransform[ int(_v.BlendIndices_.x) ], _v.Position_ ) * _v.BlendWeights_.x;				\
-		_o += mul( uBoneTransform[ int(_v.BlendIndices_.y) ], _v.Position_ ) * _v.BlendWeights_.y;				\
-		_o += mul( uBoneTransform[ int(_v.BlendIndices_.z) ], _v.Position_ ) * _v.BlendWeights_.z;				\
-		_o += mul( uBoneTransform[ int(_v.BlendIndices_.w) ], _v.Position_ ) * _v.BlendWeights_.w;				\
+#  define PSY_MAKE_WORLD_SPACE_VERTEX( _o, _v, _p ) 																\
+		_o = float4( 0.0, 0.0, 0.0, 0.0 );																\
+		_o += mul( uBoneTransform[ int(_p.BlendIndices_.x) ], _v ) * _p.BlendWeights_.x;				\
+		_o += mul( uBoneTransform[ int(_p.BlendIndices_.y) ], _v ) * _p.BlendWeights_.y;				\
+		_o += mul( uBoneTransform[ int(_p.BlendIndices_.z) ], _v ) * _p.BlendWeights_.z;				\
+		_o += mul( uBoneTransform[ int(_p.BlendIndices_.w) ], _v ) * _p.BlendWeights_.w;				\
 
 
 #elif defined( PERM_MESH_PARTICLE )
@@ -20,41 +32,33 @@
 /**
  * Make a world space vertex.
  * @param _o Output vertex. Should be float4.
- * @param _v Input vertex. Should be a structure containing Position_, VertexOffset_
+ * @param _v Input vertex. Should be float4.
+ * @param _p Input properties. Should be a structure containing VertexOffset_.
  */
-#  define PSY_MAKE_WORLD_SPACE_VERTEX( _o, _v ) 																\
-		_o = _v.Position_ + 																					\
-				float4(																							\
-					mul(																						\
-						float3x3(																				\
-							uInverseViewTransform[0].xyz, 														\
-							uInverseViewTransform[1].xyz, 														\
-							uInverseViewTransform[2].xyz ),														\
-			 			_v.VertexOffset_.xyz ),	0.0 );															\
+#  define PSY_MAKE_WORLD_SPACE_VERTEX( _o, _v, _p ) 																\
+		_o = _v + 																						\
+				float4(																					\
+					mul(																				\
+						float3x3(																		\
+							uInverseViewTransform[0].xyz, 												\
+							uInverseViewTransform[1].xyz, 												\
+							uInverseViewTransform[2].xyz ),												\
+			 			_p.VertexOffset_.xyz ),	0.0 );													\
 
 #elif defined( PERM_MESH_INSTANCED )
 
 /**
  * Make a world space vertex.
  * @param _o Output vertex. Should be float4.
- * @param _v Input vertex. Should be a structure containing Position_ and WorldMatrix[0-3].
+ * @param _v Input vertex. Should be float4.
+ * @param _p Input properties. Should be a structure containing WorldMatrix[0-3].
  */
-#  define PSY_MAKE_WORLD_SPACE_VERTEX( _o, _v ) 																\
-		_o = mul( 																								\
-				float4x4(  																						\
-					_v.WorldMatrix0_, 																			\
-					_v.WorldMatrix1_, 																			\
-					_v.WorldMatrix2_, 																			\
-					_v.WorldMatrix3_ ), _v.Position_ );	 														\
-
-#else
-
-/**
- * Make a world space vertex.
- * @param _o Output vertex. Should be float4.
- * @param _v Input vertex. Should be a structure containing Position_.
- */
-#  define PSY_MAKE_WORLD_SPACE_VERTEX( _o, _v ) 																\
-		_o = mul( uWorldTransform, _v.Position_ );																\
+#  define PSY_MAKE_WORLD_SPACE_VERTEX( _o, _v, _p ) 													\
+		_o = mul( 																						\
+				float4x4(  																				\
+					_p.WorldMatrix0_, 																	\
+					_p.WorldMatrix1_, 																	\
+					_p.WorldMatrix2_, 																	\
+					_p.WorldMatrix3_ ), _v );	 														\
 
 #endif
