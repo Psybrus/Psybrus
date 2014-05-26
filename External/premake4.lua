d53ed84e237675a1f9c61130aaca52df80650c56
Neil Richardson <neilo@neilo.gd>
2014-05-26 21:47:20 +1000

    Updated premake to move the build file out of HLSLCrossCompiler's folder so it doesn't end up part of its repo

--- External/premake4.lua
+++ External/premake4.lua
@@ -1,8 +1,8 @@
+-- In repo.
 dofile ("bullet-2.81-rev2613/premake4.lua")
 dofile ("enet/premake4.lua")
 dofile ("freetype/premake4.lua")
 dofile ("glew/premake4.lua")
-dofile ("HLSLCrossCompiler/premake4.lua")
 dofile ("jsoncpp/premake4.lua")
 dofile ("libb64/premake4.lua")
 dofile ("webby/premake4.lua")
@@ -13,3 +13,6 @@ dofile ("portaudio/premake4.lua")
 dofile ("squish/premake4.lua")
 dofile ("tremor/premake4.lua")
 dofile ("zlib/premake4.lua")
+
+-- Submodules.
+dofile ("HLSLCrossCompiler_premake4.lua")
