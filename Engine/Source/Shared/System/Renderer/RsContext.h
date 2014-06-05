6571e88c80daa16dc66984de1d464549f885f950
Neil Richardson <neilo@neilo.gd>
2014-06-05 21:13:59 +1000

    Moved all draw specific stuff from RsContext into RsDrawInterface.

--- Engine/Source/Shared/System/Renderer/RsContext.h
+++ Engine/Source/Shared/System/Renderer/RsContext.h
@@ -16,6 +16,7 @@
 
 #include "System/Renderer/RsTypes.h"
 #include "System/Renderer/RsResource.h"
+#include "System/Renderer/RsDrawInterface.h"
 
 //////////////////////////////////////////////////////////////////////////
 // Forward Declarations
@@ -24,7 +25,8 @@ class OsClient;
 //////////////////////////////////////////////////////////////////////////
 // RsContext
 class RsContext:
-	public RsResource
+	public RsResource,
+	public RsDrawInterface
 {
 public:
 	enum
@@ -62,77 +64,6 @@ public:
 	 * Requests a screenshot from the context.
 	 */
 	virtual void takeScreenshot() = 0;
-
-	/**
-	 * Set default state.
-	 */
-	virtual void setDefaultState() = 0;
-	
-	/**
-	 * Invalidate render state.
-	 */
-	virtual void invalidateRenderState() = 0;
-	
-	/**
-	 * Invalidate texture state.
-	 */
-	virtual void invalidateTextureState() = 0;
-	
-	/**
-	 * Set render state.
-	 */
-	virtual void setRenderState( RsRenderStateType State, BcS32 Value, BcBool Force = BcFalse ) = 0;
-	
-	/**
-	 * Get render state.
-	 */
-	virtual BcS32 getRenderState( RsRenderStateType State ) const = 0;
-
-	/**
-	 * Set texture state.
-	 */
-	virtual void setTextureState( BcU32 Sampler, class RsTexture* pTexture, const RsTextureParams& Params, BcBool Force = BcFalse ) = 0;
-
-	/**
-	 * Set program.
-	 */
-	virtual void setProgram( class RsProgram* Program ) = 0;
-
-	/**
-	 * Set index buffer.
-	 */
-	virtual void setIndexBuffer( class RsIndexBuffer* IndexBuffer ) = 0;
-
-	/**
-	 * Set vertex buffer.
-	 */
-	virtual void setVertexBuffer( BcU32 StreamIdx, class RsVertexBuffer* VertexBuffer ) = 0;
-
-	/**
-	 * Set vertex declaration.
-	 */
-	virtual void setVertexDeclaration( class RsVertexDeclaration* VertexDeclaration ) = 0;
-
-	/**
-	 * Flush state.
-	 */
-	virtual void flushState() = 0;
-
-	/**
-	 * Clear.
-	 */
-	virtual void clear( const RsColour& Colour ) = 0;
-	
-	/**
-	 * Draw primitives.
-	 */
-	virtual void drawPrimitives( RsPrimitiveType PrimitiveType, BcU32 IndexOffset, BcU32 NoofIndices ) = 0;
-
-	/**
-	 * Draw indexed primitives.
-	 */
-	virtual void drawIndexedPrimitives( RsPrimitiveType PrimitiveType, BcU32 IndexOffset, BcU32 NoofIndices, BcU32 VertexOffset ) = 0;
 };
-
 
 #endif
