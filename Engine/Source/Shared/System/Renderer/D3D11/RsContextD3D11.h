96161478bf6e9f77b1ce26fd9b7db1a77ddd4293
Neil Richardson <neilo@neilo.gd>
2014-08-15 00:19:22 +1000

    some d3d11 basics

--- Engine/Source/Shared/System/Renderer/D3D11/RsContextD3D11.h
+++ Engine/Source/Shared/System/Renderer/D3D11/RsContextD3D11.h
@@ -29,22 +29,57 @@ public:
 	RsContextD3D11( OsClient* pClient, RsContextD3D11* pParent );
 	virtual ~RsContextD3D11();
 	
-	virtual BcU32						getWidth() const;
-	virtual BcU32						getHeight() const;
+	BcU32 getWidth() const;
+	BcU32 getHeight() const;
 
-	void								swapBuffers();
-	void								takeScreenshot();
+	BcBool isShaderCodeTypeSupported( RsShaderCodeType CodeType ) const;
+	RsShaderCodeType maxShaderCodeType( RsShaderCodeType CodeType ) const;
+	void takeScreenshot();
+	void setViewport( class RsViewport& Viewport );
 
-	void								setDefaultState();
-	void								invalidateRenderState();
-	void								invalidateTextureState();
-	void								setRenderState( eRsRenderState State, BcS32 Value, BcBool Force = BcFalse );
-	BcS32								getRenderState( eRsRenderState State ) const;
-	void								setTextureState( BcU32 Sampler, class RsTexture* pTexture, const RsTextureParams& Params, BcBool Force = BcFalse );
-	void								flushState();
+	void setDefaultState();
+	void invalidateRenderState();
+	void invalidateTextureState();
+	void setRenderState( RsRenderStateType State, BcS32 Value, BcBool Force = BcFalse );
+	BcS32 getRenderState( RsRenderStateType State ) const;
+	void setTextureState( BcU32 Sampler, class RsTexture* pTexture, const RsTextureParams& Params, BcBool Force = BcFalse );
+	void setProgram( class RsProgram* Program );
+	void setIndexBuffer( class RsBuffer* IndexBuffer );
+	void setVertexBuffer( 
+		BcU32 StreamIdx, 
+		class RsBuffer* VertexBuffer,
+		BcU32 Stride );
+	void setUniformBuffer( 
+		BcU32 SlotIdx, 
+		class RsBuffer* UniformBuffer );
+	void setVertexDeclaration( class RsVertexDeclaration* VertexDeclaration );
+	void clear( const RsColour& Colour );
+	void drawPrimitives( RsTopologyType PrimitiveType, BcU32 IndexOffset, BcU32 NoofIndices );
+	void drawIndexedPrimitives( RsTopologyType PrimitiveType, BcU32 IndexOffset, BcU32 NoofIndices, BcU32 VertexOffset );
 
-	ID3D11Device*						device();
-	ID3D11DeviceContext*				deviceContext();
+	bool createBuffer( 
+		RsBuffer* Buffer );
+	bool destroyBuffer( 
+		RsBuffer* Buffer );
+	bool updateBuffer( 
+		RsBuffer* Buffer,
+		BcSize Offset,
+		BcSize Size,
+		RsResourceUpdateFlags Flags,
+		RsBufferUpdateFunc UpdateFunc );
+
+	bool createTexture( 
+		class RsTexture* Texture );
+	bool destroyTexture( 
+		class RsTexture* Texture );
+	bool updateTexture( 
+		class RsTexture* Texture,
+		const struct RsTextureSlice& Slice,
+		RsResourceUpdateFlags Flags,
+		RsTextureUpdateFunc UpdateFunc );
+		
+	void flushState();
+
 
 protected:
 	virtual void						create();
