1a13cc79ed6a75aa92ce47fa445080f55dea8645
Neil Richardson <neilo@neilo.gd>
2014-08-15 01:04:56 +1000

    added texture resources and fixed the buffer to create correctly.

--- Engine/Source/Shared/System/Renderer/D3D11/RsContextD3D11.cpp
+++ Engine/Source/Shared/System/Renderer/D3D11/RsContextD3D11.cpp
@@ -26,6 +26,44 @@
 #pragma comment (lib, "d3d11.lib")
 
 //////////////////////////////////////////////////////////////////////////
+// Type conversion.
+static D3D11_BIND_FLAG gBufferType[] =
+{
+	(D3D11_BIND_FLAG)0,			// RsBufferType::UNKNOWN
+	D3D11_BIND_VERTEX_BUFFER,	// RsBufferType::VERTEX
+	D3D11_BIND_INDEX_BUFFER,	// RsBufferType::INDEX
+	D3D11_BIND_CONSTANT_BUFFER,	// RsBufferType::UNIFORM
+	D3D11_BIND_UNORDERED_ACCESS,// RsBufferType::UNORDERED_ACCESS
+	(D3D11_BIND_FLAG)0,			// RsBufferType::DRAW_INDIRECT
+	D3D11_BIND_STREAM_OUTPUT,	// RsBufferType::STREAM_OUT
+};
+
+static DXGI_FORMAT gTextureFormats[] =
+{
+	// Colour.
+	DXGI_FORMAT_R8_UNORM,				// RsTextureFormat::R8,
+	DXGI_FORMAT_R8G8_UNORM,				// RsTextureFormat::R8G8,
+	DXGI_FORMAT_UNKNOWN,				// RsTextureFormat::R8G8B8,
+	DXGI_FORMAT_R8G8B8A8_UNORM,			// RsTextureFormat::R8G8B8A8,
+	DXGI_FORMAT_R16_FLOAT,				// RsTextureFormat::R16F,
+	DXGI_FORMAT_R16G16_FLOAT,			// RsTextureFormat::R16FG16F,
+	DXGI_FORMAT_UNKNOWN,				// RsTextureFormat::R16FG16FB16F,
+	DXGI_FORMAT_R16G16B16A16_FLOAT,		// RsTextureFormat::R16FG16FB16FA16F,
+	DXGI_FORMAT_R32_FLOAT,				// RsTextureFormat::R32F,
+	DXGI_FORMAT_R32G32_FLOAT,			// RsTextureFormat::R32FG32F,
+	DXGI_FORMAT_UNKNOWN,				// RsTextureFormat::R32FG32FB32F,
+	DXGI_FORMAT_R32G32B32A32_FLOAT,		// RsTextureFormat::R32FG32FB32FA32F,
+	DXGI_FORMAT_BC1_UNORM,				// RsTextureFormat::DXT1,
+	DXGI_FORMAT_BC3_UNORM,				// RsTextureFormat::DXT3,
+	DXGI_FORMAT_BC5_UNORM,				// RsTextureFormat::DXT5,
+	DXGI_FORMAT_D16_UNORM,				// RsTextureFormat::D16,
+	DXGI_FORMAT_UNKNOWN,				// RsTextureFormat::D32,
+	DXGI_FORMAT_D24_UNORM_S8_UINT,		// RsTextureFormat::D24S8,
+	DXGI_FORMAT_UNKNOWN,				// RsTextureFormat::D32F,
+
+};
+
+//////////////////////////////////////////////////////////////////////////
 // Ctor
 RsContextD3D11::RsContextD3D11( OsClient* pClient, RsContextD3D11* pParent ):
 	RsContext( pParent ),
@@ -290,14 +328,26 @@ bool RsContextD3D11::createBuffer(
 {
 	BcAssertMsg( BcCurrentThreadId() == OwningThread_, "Calling context calls from invalid thread." );
 
+	//
+	const auto& BufferDesc = Buffer->getDesc();
+
 	// Buffer desc.
 	D3D11_BUFFER_DESC Desc;
 	Desc.Usage = D3D11_USAGE_DEFAULT;			// TODO.
-	Desc.ByteWidth = Buffer->getDesc().SizeBytes_;
-	Desc.BindFlags = D3D11_BIND_INDEX_BUFFER;	// TODO.
+	Desc.ByteWidth =BufferDesc.SizeBytes_;
+	Desc.BindFlags = gBufferType[ (BcU32)BufferDesc.Type_ ];
 	Desc.CPUAccessFlags = 0;
 	Desc.MiscFlags = 0;
 	
+	if( ( BufferDesc.Flags_ & RsResourceCreationFlags::DYNAMIC ) != RsResourceCreationFlags::NONE )
+	{
+		Desc.Usage = D3D11_USAGE_DYNAMIC;
+	}
+	else if( ( BufferDesc.Flags_ & RsResourceCreationFlags::STREAM ) != RsResourceCreationFlags::NONE )
+	{
+		Desc.Usage = D3D11_USAGE_DYNAMIC;
+	}
+
 	// Generate buffers.
 	ID3D11Buffer* D3DBuffer = nullptr; 
 
@@ -319,8 +369,14 @@ bool RsContextD3D11::destroyBuffer(
 	BcAssertMsg( BcCurrentThreadId() == OwningThread_, "Calling context calls from invalid thread." );
 
 	ID3D11Buffer* D3DBuffer = Buffer->getHandle< ID3D11Buffer* >(); 
-	D3DBuffer->Release();
-	return true;
+
+	if( D3DBuffer != nullptr )
+	{
+		D3DBuffer->Release();
+		return true;
+	}
+
+	return false;	
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -334,7 +390,26 @@ bool RsContextD3D11::updateBuffer(
 {
 	BcAssertMsg( BcCurrentThreadId() == OwningThread_, "Calling context calls from invalid thread." );
 
-	BcBreakpoint;
+	ID3D11Buffer* D3DBuffer = Buffer->getHandle< ID3D11Buffer* >(); 
+
+	if( D3DBuffer != nullptr )
+	{
+		D3D11_MAP MapType = 
+			D3D11_MAP_WRITE;		// TODO: Select more optimal map type.
+		D3D11_MAPPED_SUBRESOURCE SubRes = { nullptr, 0, 0 };
+		HRESULT RetVal = Context_->Map( D3DBuffer, 0, MapType, 0, &SubRes );
+		if( SUCCEEDED( RetVal ) )
+		{
+			RsBufferLock Lock = 
+			{
+				(BcU8*)SubRes.pData + Offset
+			};
+			UpdateFunc( Buffer, Lock );
+			Context_->Unmap( D3DBuffer, 0 );
+		}
+		return true;
+	}
+
 	return false;
 }
 
@@ -345,7 +420,96 @@ bool RsContextD3D11::createTexture(
 {
 	BcAssertMsg( BcCurrentThreadId() == OwningThread_, "Calling context calls from invalid thread." );
 
-	BcBreakpoint;
+	//
+	const auto& TextureDesc = Texture->getDesc();
+
+	// Buffer desc.
+	switch( TextureDesc.Type_ )
+	{
+	case RsTextureType::TEX1D:
+		{
+			D3D11_TEXTURE1D_DESC Desc;
+			Desc.Width = TextureDesc.Width_;
+			Desc.MipLevels = TextureDesc.Levels_;
+			Desc.ArraySize = 1;
+			Desc.Format = gTextureFormats[ (BcU32)TextureDesc.Format_ ];
+			Desc.Usage = D3D11_USAGE_DEFAULT;
+			Desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+			Desc.CPUAccessFlags = 0;
+			Desc.MiscFlags = 0;
+
+			ID3D11Texture1D* D3DTexture = nullptr;
+			Device_->CreateTexture1D( &Desc, nullptr, &D3DTexture );
+
+			Texture->setHandle( D3DTexture );
+		}
+		break;
+
+	case RsTextureType::TEX2D:
+		{
+			D3D11_TEXTURE2D_DESC Desc;
+			Desc.Width = TextureDesc.Width_;
+			Desc.Height = TextureDesc.Height_;
+			Desc.MipLevels = TextureDesc.Levels_;
+			Desc.ArraySize = 1;
+			Desc.Format = gTextureFormats[ (BcU32)TextureDesc.Format_ ];
+			Desc.SampleDesc.Count = 0;
+			Desc.SampleDesc.Quality = 0;
+			Desc.Usage = D3D11_USAGE_DEFAULT;
+			Desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+			Desc.CPUAccessFlags = 0;
+			Desc.MiscFlags = 0;
+
+			ID3D11Texture2D* D3DTexture = nullptr;
+			Device_->CreateTexture2D( &Desc, nullptr, &D3DTexture );
+
+			Texture->setHandle( D3DTexture );
+		}
+		break;
+
+	case RsTextureType::TEX3D:
+		{
+			D3D11_TEXTURE3D_DESC Desc;
+			Desc.Width = TextureDesc.Width_;
+			Desc.Height = TextureDesc.Height_;
+			Desc.Depth = TextureDesc.Depth_;
+			Desc.MipLevels = TextureDesc.Levels_;
+			Desc.Format = gTextureFormats[ (BcU32)TextureDesc.Format_ ];
+			Desc.Usage = D3D11_USAGE_DEFAULT;
+			Desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+			Desc.CPUAccessFlags = 0;
+			Desc.MiscFlags = D3D11_RESOURCE_MISC_TEXTURECUBE;
+
+			ID3D11Texture3D* D3DTexture = nullptr;
+			Device_->CreateTexture3D( &Desc, nullptr, &D3DTexture );
+
+			Texture->setHandle( D3DTexture );
+		}
+		break;
+
+	case RsTextureType::TEXCUBE:
+		{
+			D3D11_TEXTURE2D_DESC Desc;
+			Desc.Width = TextureDesc.Width_;
+			Desc.Height = TextureDesc.Height_;
+			Desc.MipLevels = TextureDesc.Levels_;
+			Desc.ArraySize = 1;
+			Desc.Format = gTextureFormats[ (BcU32)TextureDesc.Format_ ];
+			Desc.SampleDesc.Count = 0;
+			Desc.SampleDesc.Quality = 0;
+			Desc.Usage = D3D11_USAGE_DEFAULT;
+			Desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+			Desc.CPUAccessFlags = 0;
+			Desc.MiscFlags = 0;
+
+			ID3D11Texture2D* D3DTexture = nullptr;
+			Device_->CreateTexture2D( &Desc, nullptr, &D3DTexture );
+
+			Texture->setHandle( D3DTexture );
+		}
+		break;
+	}
+	
 	return false;
 }
 
@@ -356,8 +520,15 @@ bool RsContextD3D11::destroyTexture(
 {
 	BcAssertMsg( BcCurrentThreadId() == OwningThread_, "Calling context calls from invalid thread." );
 
-	BcBreakpoint;
-	return false;
+	ID3D11Resource* D3DResource = Texture->getHandle< ID3D11Resource* >(); 
+
+	if( D3DResource != nullptr )
+	{
+		D3DResource->Release();
+		return true;
+	}
+
+	return false;	
 }
 
 //////////////////////////////////////////////////////////////////////////
