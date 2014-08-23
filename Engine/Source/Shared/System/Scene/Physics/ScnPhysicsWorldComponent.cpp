f3d5c6d02171a74df874d9bbe40b4cc1e93970c7
Neil Richardson <neilo@neilo.gd>
2014-08-23 13:57:00 +1000

    debug physics rendering

--- Engine/Source/Shared/System/Scene/Physics/ScnPhysicsWorldComponent.cpp
+++ Engine/Source/Shared/System/Scene/Physics/ScnPhysicsWorldComponent.cpp
@@ -15,10 +15,61 @@
 #include "System/Scene/ScnEntity.h"
 
 #include "System/Content/CsCore.h"
+#include "System/Debug/DsCore.h"
+#include "System/Scene/ScnDebugRenderComponent.h"
 
 #include "btBulletCollisionCommon.h"
 #include "btBulletDynamicsCommon.h"
 
+#include "LinearMath/btIDebugDraw.h"
+
+class DebugRenderer: 
+	public btIDebugDraw
+{
+public:
+	DebugRenderer()
+	{
+		DebugMode_ = btIDebugDraw::DBG_DrawWireframe;
+	}
+
+	virtual void drawLine(const btVector3& from,const btVector3& to,const btVector3& color)
+	{
+		ScnDebugRenderComponent::pImpl()->drawLine(
+			MaVec3d( from.x(), from.y(), from.z() ),
+			MaVec3d( to.x(), to.y(), to.z() ),
+			RsColour( color.x(), color.y(), color.z(), 1.0f ) );
+	}
+
+	virtual void drawContactPoint(const btVector3& PointOnB,const btVector3& normalOnB,btScalar distance,int lifeTime,const btVector3& color)
+	{
+
+	}
+
+	virtual void reportErrorWarning(const char* warningString)
+	{
+
+	}
+
+	virtual void draw3dText(const btVector3& location,const char* textString)
+	{
+
+	}
+	
+	virtual void setDebugMode(int debugMode)
+	{
+		DebugMode_ = debugMode;
+	}
+	
+	virtual int getDebugMode() const
+	{
+		return DebugMode_;
+	}
+
+private:
+	int DebugMode_;
+};
+
+static DebugRenderer gDebugRenderer;
 
 //////////////////////////////////////////////////////////////////////////
 // Define resource internals.
@@ -42,6 +93,7 @@ void ScnPhysicsWorldComponent::initialise()
 	Broadphase_ = nullptr;
 	Solver_ = nullptr;
 	DynamicsWorld_ = nullptr;
+	DebugDrawWorld_ = BcFalse;
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -96,6 +148,10 @@ void ScnPhysicsWorldComponent::update( BcF32 Tick )
 //virtual
 void ScnPhysicsWorldComponent::postUpdate( BcF32 Tick )
 {
+	if( DebugDrawWorld_ )
+	{
+		DynamicsWorld_->debugDrawWorld();
+	}
 
 	Super::postUpdate( Tick );
 }
@@ -113,6 +169,21 @@ void ScnPhysicsWorldComponent::onAttach( ScnEntityWeakRef Parent )
 	DynamicsWorld_ = new btDiscreteDynamicsWorld( Dispatcher_, Broadphase_, Solver_, CollisionConfiguration_ );
 	DynamicsWorld_->setGravity( btVector3( 0.0f, -10.0f, 0.0f ) );
 
+	DynamicsWorld_->setDebugDrawer( &gDebugRenderer );
+
+#if defined( PSY_DEBUG )
+	DebugDrawWorld_ = BcTrue;
+#endif
+
+	if( DsCore::pImpl() )
+	{
+		DsCore::pImpl()->registerFunction("Toggle Debug Physics Rendering",
+			[ this ]
+			{
+				DebugDrawWorld_ = !DebugDrawWorld_;
+			});
+	}
+
 	Super::onAttach( Parent );
 }
 
@@ -121,6 +192,11 @@ void ScnPhysicsWorldComponent::onAttach( ScnEntityWeakRef Parent )
 //virtual
 void ScnPhysicsWorldComponent::onDetach( ScnEntityWeakRef Parent )
 {
+	if( DsCore::pImpl() )
+	{
+		DsCore::pImpl()->deregisterFunction("Toggle Debug Physics Rendering");
+	}
+
 	delete DynamicsWorld_;
 	delete Solver_;
 	delete Broadphase_;
