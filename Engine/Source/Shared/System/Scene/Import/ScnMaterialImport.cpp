fdf89801810cdf818402669601449d3a04ac2f5e
Neil Richardson <neilo@neilo.gd>
2014-08-30 22:12:17 +1000

    material no longer parsing json manually. Still need to clean up the render state setting + implement sampler state.

--- Engine/Source/Shared/System/Scene/Import/ScnMaterialImport.cpp
+++ Engine/Source/Shared/System/Scene/Import/ScnMaterialImport.cpp
@@ -20,27 +20,46 @@
 #include "Base/BcFile.h"
 #include "Base/BcStream.h"
 
+#include <boost/lexical_cast.hpp>
+
 //////////////////////////////////////////////////////////////////////////
 // Reflection
 REFLECTION_DEFINE_DERIVED( ScnMaterialImport )
 	
 void ScnMaterialImport::StaticRegisterClass()
 {
-	/*
 	ReField* Fields[] = 
 	{
-		new ReField( "Source_", &ScnMaterialImport::Source_ ),
+		new ReField( "Shader_", &ScnMaterialImport::Shader_, bcRFF_IMPORTER ),
+		new ReField( "Textures_", &ScnMaterialImport::Textures_, bcRFF_IMPORTER ),
+		new ReField( "State_", &ScnMaterialImport::State_, bcRFF_IMPORTER ),
 	};
-	*/
 		
-	ReRegisterClass< ScnMaterialImport, Super >();
+	ReRegisterClass< ScnMaterialImport, Super >( Fields );
 }
 
 //////////////////////////////////////////////////////////////////////////
 // Ctor
 ScnMaterialImport::ScnMaterialImport()
 {
-
+	// Awful default state for now.
+	DefaultState_[ (BcU32)RsRenderStateType::DEPTH_WRITE_ENABLE ] = 0;
+	DefaultState_[ (BcU32)RsRenderStateType::DEPTH_TEST_ENABLE ] = 0;
+	DefaultState_[ (BcU32)RsRenderStateType::DEPTH_TEST_COMPARE ] = 0;
+	DefaultState_[ (BcU32)RsRenderStateType::STENCIL_WRITE_MASK ] = 0;
+	DefaultState_[ (BcU32)RsRenderStateType::STENCIL_TEST_ENABLE ] = 0;
+	DefaultState_[ (BcU32)RsRenderStateType::STENCIL_TEST_FUNC_COMPARE ] = 0;
+	DefaultState_[ (BcU32)RsRenderStateType::STENCIL_TEST_FUNC_REF ] = 0;
+	DefaultState_[ (BcU32)RsRenderStateType::STENCIL_TEST_FUNC_MASK ] = 0;
+	DefaultState_[ (BcU32)RsRenderStateType::STENCIL_TEST_OP_SFAIL ] = 0;
+	DefaultState_[ (BcU32)RsRenderStateType::STENCIL_TEST_OP_DPFAIL ] = 0;
+	DefaultState_[ (BcU32)RsRenderStateType::STENCIL_TEST_OP_DPPASS ] = 0;
+	DefaultState_[ (BcU32)RsRenderStateType::COLOR_WRITE_MASK_0 ] = 0xf;
+	DefaultState_[ (BcU32)RsRenderStateType::COLOR_WRITE_MASK_1 ] = 0;
+	DefaultState_[ (BcU32)RsRenderStateType::COLOR_WRITE_MASK_2 ] = 0;
+	DefaultState_[ (BcU32)RsRenderStateType::COLOR_WRITE_MASK_3 ] = 0;
+	DefaultState_[ (BcU32)RsRenderStateType::BLEND_MODE ] = 0;
+	DefaultState_[ (BcU32)RsRenderStateType::FILL_MODE ] = 0;
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -54,12 +73,8 @@ ScnMaterialImport::~ScnMaterialImport()
 //////////////////////////////////////////////////////////////////////////
 // import
 BcBool ScnMaterialImport::import(
-		const Json::Value& Object )
-{
-	const Json::Value& ImportShader = Object[ "shader" ];
-	const Json::Value& ImportTextures = Object[ "textures" ];
-	const Json::Value& State = Object[ "state" ];
-	
+		const Json::Value& )
+{	
 	// Export material.
 	BcStream HeaderStream;
 	BcStream StateBlockStream;
@@ -68,47 +83,22 @@ BcBool ScnMaterialImport::import(
 	ScnMaterialTextureHeader TextureHeader;
 	
 	// Make header.
-	Json::Value::Members TextureMembers = ImportTextures.getMemberNames();
-
-	Header.ShaderRef_ = ImportShader.asUInt();	// TODO: Go via addImport. This can then verify for us.
-	Header.NoofTextures_ = (BcU32)TextureMembers.size();	
+	Header.ShaderRef_ = Shader_;
+	Header.NoofTextures_ = (BcU32)Textures_.size();	
 	HeaderStream << Header;
 
 	// Make texture headers.
-	for( BcU32 Idx = 0; Idx < TextureMembers.size(); ++Idx )
+	for( const auto& Texture : Textures_ )
 	{
-		const Json::Value& Texture = ImportTextures[ TextureMembers[ Idx ] ];
-
-		TextureHeader.SamplerName_ = CsResourceImporter::addString( BcName::StripInvalidChars( TextureMembers[ Idx ].c_str() ).c_str() );
-		TextureHeader.TextureRef_ = Texture.asUInt(); // TODO: Go via addImport. This can then verify for us.
+		TextureHeader.SamplerName_ = 
+			CsResourceImporter::addString( BcName::StripInvalidChars( Texture.first.c_str() ).c_str() );
+		TextureHeader.TextureRef_ = Texture.second;
 		HeaderStream << TextureHeader;
 	}
 	
-	// Make state block stream.
-	const BcChar* StateNames[] = 
-	{
-		"depth_write_enable",
-		"depth_test_enable",
-		"depth_test_compare",
-		"stencil_write_mask",
-		"stencil_test_enable",
-		"stencil_test_func_compare",
-		"stencil_test_func_ref",
-		"stencil_test_func_mask",
-		"stencil_test_op_sfail",
-		"stencil_test_op_dpfail",
-		"stencil_test_op_dppass",
-		"color_write_mask_0",
-		"color_write_mask_1",
-		"color_write_mask_2",
-		"color_write_mask_3",
-		"blend_mode",
-		"fill_mode",
-	};
-	
 	// This code is bad code. Use the reflection system.
 	std::map< std::string, BcU32 > ModeNames;
-		
+	
 	ModeNames[ "never" ] = (BcU32)RsCompareMode::NEVER;
 	ModeNames[ "less" ] = (BcU32)RsCompareMode::LESS;
 	ModeNames[ "equal" ] = (BcU32)RsCompareMode::EQUAL;
@@ -116,7 +106,7 @@ BcBool ScnMaterialImport::import(
 	ModeNames[ "greater" ] = (BcU32)RsCompareMode::GREATER;
 	ModeNames[ "notequal" ] = (BcU32)RsCompareMode::NOTEQUAL;
 	ModeNames[ "always" ] = (BcU32)RsCompareMode::ALWAYS;
-		
+	
 	ModeNames[ "none" ] = (BcU32)RsBlendingMode::NONE;
 	ModeNames[ "blend" ] = (BcU32)RsBlendingMode::BLEND;
 	ModeNames[ "add" ] = (BcU32)RsBlendingMode::ADD;
@@ -136,52 +126,24 @@ BcBool ScnMaterialImport::import(
 
 	for( BcU32 Idx = 0; Idx < (BcU32)RsRenderStateType::MAX; ++Idx )
 	{
-		bool WriteDefaultState = BcFalse;
-		if( State.type() == Json::objectValue )
+		if( State_.find( (RsRenderStateType)Idx ) != State_.end() )
 		{
-			const Json::Value& StateValue = State[ StateNames[ Idx ] ];
+			const std::string& StateValue = State_[ (RsRenderStateType)Idx ];
 			
-			if( StateValue.type() == Json::realValue )
+			if( ModeNames.find( StateValue ) != ModeNames.end() )
 			{
-				BcF32 RealValue = (BcF32)StateValue.asDouble();
-				StateBlockStream << BcU32( RealValue );
-			}
-			else if( StateValue.type() == Json::stringValue )
-			{
-				BcU32 IntValue = ModeNames[ StateValue.asCString() ];
-				StateBlockStream << BcU32( IntValue );
-			}
-			else if( StateValue.type() == Json::intValue ||
-					 StateValue.type() == Json::uintValue )
-			{
-				BcU32 IntValue = (BcU32)StateValue.asInt();
+				BcU32 IntValue = ModeNames[ StateValue ];
 				StateBlockStream << BcU32( IntValue );
 			}
 			else
 			{
-				BcPrintf( "ScnMaterial::import: Unable to parse value for \"%s\"\n", StateNames[ Idx ] );
-				WriteDefaultState = BcTrue;
+				BcU32 IntValue = boost::lexical_cast< BcU32 >( StateValue );
+				StateBlockStream << BcU32( IntValue );
 			}
 		}
 		else
 		{
-			WriteDefaultState = BcTrue;
-		}
-
-		if( WriteDefaultState )
-		{
-			// Horrible default special case. Should have a table.
-			switch( Idx )
-			{
-			case RsRenderStateType::COLOR_WRITE_MASK_0:
-				StateBlockStream << BcU32( 0xf );
-				break;
-
-			default:
-				StateBlockStream << BcU32( 0 );
-				break;
-			}
-				
+			StateBlockStream << DefaultState_[ Idx ];
 		}
 	}
 	
