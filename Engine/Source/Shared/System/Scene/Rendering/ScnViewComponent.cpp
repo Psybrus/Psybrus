976e8e52d530013263af3e31d81647b71487fff5
Neil Richardson <neilo@neilo.gd>
2014-09-14 14:52:47 +1000

    Hack up clear colour.

--- Engine/Source/Shared/System/Scene/Rendering/ScnViewComponent.cpp
+++ Engine/Source/Shared/System/Scene/Rendering/ScnViewComponent.cpp
@@ -265,7 +265,7 @@ public:
 	{
 		PSY_PROFILER_SECTION( RenderRoot, "ScnViewComponentViewport::render" );
 		pContext_->setViewport( Viewport_ );
-		pContext_->clear( RsColour::BLACK );
+		pContext_->clear( RsColour::WHITE );
 	}
 
 	RsViewport Viewport_;
